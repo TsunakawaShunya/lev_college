3236d2673f45d6bf9e964f71002f10b79e688683
TsunakawaShunya <uu.tsunakawa@gmail.com>
2024-03-16 17:51:00 +0900

    breeze導入

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,42 +1,33 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <title>Blog</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    </head>
-    <body>
-        <h1>Blog Name</h1>
-        <div class='posts'>
-            @foreach ($posts as $post)
-                <h2 class='title'>
-                    <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
-                </h2>
-                <p class='body'>{{ $post->body }}</p>
-                <!-- 以下を追記 -->
-                <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
-                    @csrf
-                    @method('DELETE')
-                    <button type="button" onclick="deletePost({{ $post->id }})">delete</button> 
-                </form>
-                <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
-            @endforeach
-        </div>
-        <div class='paginate'>
-            {{ $posts->links() }}
-        </div>
-        <a href='/posts/create'>create</a>
-        
-        <script>
-            function deletePost(id) {
-                'use strict'
-        
-                if (confirm('削除すると復元できません。\n本当に削除しますか？')) {
-                    document.getElementById(`form_${id}`).submit();
-                }
+<x-app-layout>
+    <x-slot name="header">作成</x-slot>
+    <h1>Blog Name</h1>
+    <div class='posts'>
+        @foreach ($posts as $post)
+            <h2 class='title'>
+                <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+            </h2>
+            <p class='body'>{{ $post->body }}</p>
+            <!-- 以下を追記 -->
+            <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+                @csrf
+                @method('DELETE')
+                <button type="button" onclick="deletePost({{ $post->id }})">delete</button> 
+            </form>
+            <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
+        @endforeach
+    </div>
+    <div class='paginate'>
+        {{ $posts->links() }}
+    </div>
+    <a href='/posts/create'>create</a>
+    
+    <script>
+        function deletePost(id) {
+            'use strict'
+    
+            if (confirm('削除すると復元できません。\n本当に削除しますか？')) {
+                document.getElementById(`form_${id}`).submit();
             }
-        </script>    
-    </body>
-</html>
-
+        }
+    </script>    
+</x-app-layout>
